94ad05b61c717308c397c1257bdc24f99ef5079e
petushka1 <nadya@devery.io>
2022-06-13 18:09:31 +0300

    push the project

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
-# to-do-list-review
-a cloned to-do-list created for peer-to-peer review practise.
+![](https://img.shields.io/badge/Microverse-blueviolet)
+
+# To Do List
+
+A Books manager webapp that lists existing records and allows user to add and remove to and from the books record.
+
+
+## Built With
+
+- html
+- css
+- JavaScript
+- Node.js
+- npm
+
+
+## Getting Started
+
+Use this repo as a reference or clone to customize for your own project.
+
+### Prerequisites
+
+- Must have a working computer
+- A Web-Browser installed (Chrome - Recommended)
+
+### Setup
+
+- Install _VSCode_ ([link](https://code.visualstudio.com/download))
+- Clone this project repository on your local computer. ([link](../../))
+- Open the project directory with _VSCode_
+
+### Install
+
+Open _Terminal/Shell_ inside your _VSCode_ and run
+  ```
+    npm install --force
+  ```
+
+### Run tests
+
+- For _hint_ tests, run
+  ```
+    npm run lint:hint
+  ```
+- For _style_ tests, run
+  ```
+    npm run lint:style
+  ```
+- For _eslint_ tests, run
+  ```
+    npm run lint:es
+  ```
+
+### Deployment
+
+
+## Authors
+
+👤 **Nadezhda**
+
+- GitHub: [@petushka1](https://github.com/petushka1)
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](../../issues/).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- Fellow Micronauts
---
+++ src/extend.js
@@ -0,0 +1,153 @@
+import { taskArr, setLocalStorage } from './storage.js';
+import dots from './dots.svg';
+import bin from './remove.svg';
+
+const LIST = document.querySelector('.list');
+
+LIST.innerHTML = '';
+
+function isListempty() {
+  if (LIST.hasChildNodes()) {
+    return true;
+  }
+
+  return false;
+}
+
+// rearrange indexes in local storage, task array and assign new IDs
+function rearrange() {
+  if (LIST.hasChildNodes()) {
+    const children = LIST.childNodes;
+
+    for (let i = 0; i < taskArr.length; i += 1) {
+      children[i].id = i;
+      taskArr.list[i].index = i;
+    }
+  }
+}
+
+// add new item
+// new task template added to newly created li
+function makeTaskElement({ description }) {
+  const li = document.createElement('li');
+  li.className = 'item';
+  li.innerHTML = `<label><input type="checkbox"><input class="edit" placeholder='${description}'></input></label><img src='${dots}'>`;
+  return li;
+}
+
+// render new task by appending this to ul
+// assign id to a new li and make a localStorage record
+export function addTask(description) {
+  const newTask = {
+    index: taskArr.length,
+    description,
+    completed: false,
+  };
+
+  taskArr.push(newTask);
+  setLocalStorage();
+
+  const newListItem = makeTaskElement(newTask);
+  newListItem.id = newTask.index;
+
+  LIST.appendChild(newListItem);
+}
+
+// remove li from the list (ul) and from task array
+function remove({ target }) {
+  const li = target.parentElement;
+  const index = parseInt(li.id, 10);
+
+  li.remove();
+  taskArr.remove(index);
+
+  rearrange();
+  setLocalStorage();
+}
+
+export function updateTaskDescription({ target }) {
+  const { value } = target;
+  target.placeholder = value;
+  target.value = '';
+
+  const index = parseInt(target.parentElement.parentElement.id, 10);
+  taskArr.list[index].description = value;
+
+  setLocalStorage();
+}
+
+// add text decoration for completed tasks
+export function taskCompleted(target) {
+  const taskCompletedLi = target.parentElement.parentElement;
+  const index = parseInt(taskCompletedLi.id, 10);
+
+  if (target.checked) {
+    taskArr.list[index].completed = true;
+    target.nextElementSibling.style['text-decoration'] = 'line-through';
+  } else {
+    taskArr.list[index].completed = false;
+    target.nextElementSibling.style['text-decoration'] = 'none';
+  }
+  setLocalStorage();
+}
+
+// clear all completed tasks on button click
+export function clearCompleted() {
+  taskArr.filter();
+
+  document.body.querySelectorAll(':checked').forEach(({ parentElement }) => {
+    const parent = parentElement.parentElement;
+    parent.remove();
+  });
+
+  rearrange();
+  setLocalStorage();
+}
+
+export function taskSelectionChange({ target: current }) {
+  const checkElement = LIST.querySelector('.active');
+
+  if (checkElement != null) {
+    checkElement.classList.remove('active');
+    const [, lastChild] = checkElement.children;
+    lastChild.src = dots;
+  }
+
+  current.classList.toggle('active');
+  const [, lastChild] = current.children;
+  lastChild.src = bin;
+
+  if (!lastChild.isBound) {
+    lastChild.addEventListener('click', remove);
+    lastChild.isBound = true;
+  }
+}
+
+export const populate = () => {
+  if (taskArr.length === 0) return;
+
+  let listItems = "";
+
+  taskArr.list.forEach(({ index, description }) => {
+    listItems += `
+      <li id='${index}' class="item">
+        <label>
+          <input type="checkbox">
+          <input type='text' class="edit" placeholder='${description}'>
+        </label>
+        <img src='${dots}'>
+      </li>
+    `;
+  });
+
+  LIST.innerHTML = listItems;
+
+  if (isListempty()) {
+    taskArr.list.forEach((item, i) => {
+      if (item.completed === true) {
+        const li = document.getElementById(`${i}`);
+        li.querySelector('input[type=checkbox]').checked = true;
+      }
+    });
+  }
+};
